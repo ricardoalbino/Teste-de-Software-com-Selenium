36d244a78c532e0c217e2d74b2630588bd67f927
Ricardo Albino <ricardo_albino83@hotmail.com>
2018-08-13 23:59:59 -0300

    fist commit

---
+++ Selenium.Tests/Testes/UsuarioSystemTest.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.Tests.Testes
+{
+    [TestFixture]
+   public class UsuarioSystemTest
+    {
+        [Test]
+        public void deveCadastrarUsuario()
+        {
+            
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://localhost:63883/Usuarios/Index");
+
+            // clique no link - Novo Usuario para levar para pagina de cadastro
+            driver.FindElement(By.LinkText("Novo Usuario")).Click();
+
+            // encontrando ambos elementos na pagina
+            IWebElement campoNome = driver.FindElement(By.Name("nome"));
+            IWebElement campoEmail = driver.FindElement(By.Name("email"));
+            IWebElement btnSalvar = driver.FindElement(By.Name("btnSalvar"));
+
+            campoNome.SendKeys("Cadu Albino");
+            campoEmail.SendKeys("cadu@hotmail.com");
+
+            btnSalvar.Click();
+
+           
+            //Teste Unitario - NUnit
+            bool encontrouNome = driver.PageSource.Contains("Cadu Albino");
+            bool encontrouEmail = driver.PageSource.Contains("cadu@hotmail.com");
+
+            Assert.IsTrue(encontrouNome);
+            Assert.IsTrue(encontrouEmail);
+
+            
+
+            driver.Close();
+            driver.FindElement(By.LinkText("Lista de Usuarios")).Click();
+
+
+        }
+    }
+}
---
+++ Selenium/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Selenium.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> DbUsuario { get; set; }
+    }
+}
